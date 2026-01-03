202b42aa9f69961ab0a95e6fad5f91e205d14c98
nyamulacoretta-ops <nyamulacoretta@gmail.com>
2026-01-03 22:26:22 +0200

    Tried to use a struct but failed. Will fix it later

--- main.go
+++ main.go
@@ -4,11 +4,36 @@ import "fmt"
 
 const pMoney int = 32
 
+type messageToSend struct {
+	phoneNumber int
+	message     string
+}
+
+type Corie struct {
+	phoneNumber int
+	address     string
+	homeTown    home
+	gender bool
+}
+type home struct {
+	district string
+	ta       string
+	village string
+	country string
+}
+
+var myDetails Corie
+myDetails.district = "Mchinji"
+
+
+func test(n messageToSend) {
+	fmt.Printf("Sending message from: '%v'\n message: %s\n", n.phoneNumber, n.message)
+	fmt.Printf("----------------\n")
+}
 func calc(pMoney int, rMoney float64) int {
 	remainingMoney := int(rMoney)
 	remainingMoney = 1200 - pMoney
 	return remainingMoney + pMoney
-
 }
 
 func main() {
@@ -24,10 +49,19 @@ func main() {
 	myMoney := calc(pMoney, rMoney)
 	statement := fmt.Sprintf("My whole inheritance is %v", myMoney)
 
+	test(messageToSend{
+		phoneNumber: 265887539584,
+		message:     "I really love you"})
+
+	test(messageToSend{
+		phoneNumber: 265981831528,
+		message:     "I really hate you"})
+
 	fmt.Println("starting Textio server")
 	fmt.Println(herName, "is ", herAgeInt)
 	fmt.Println(msg)
 	fmt.Println(statement)
+	fmt.Println(myDetails.homeTown.district)
 }
 
 //for conditionals, we use if, else if, else....
